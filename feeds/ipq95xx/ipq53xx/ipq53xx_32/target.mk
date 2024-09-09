f987216d441d0a863ea9636dfd8afff0d269681b
Karim O. <146198118+kmoz000@users.noreply.github.com>
2024-09-09 14:50:33 +0100

    - subtarget 5332 32bit

--- feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk
+++ feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk
@@ -6,7 +6,7 @@ CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
 	uboot-2016-ipq5332 -uboot-2016-ipq5332_tiny -lk-ipq53xx \
-	fwupgrade-tools sysupgrade-helper
+	fwupgrade-tools
 
 define Target/Description
 	Build firmware image for IPQ53xx SoC devices.
