312fa27f82556897eff26ce35727483933767f05
Karim O. <146198118+kmoz000@users.noreply.github.com>
2024-09-09 10:14:53 +0100

    - qcom_ipq5332-ap-mi01.12

--- feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk
+++ feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk
@@ -6,7 +6,7 @@ CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
 	uboot-2016-ipq5332 -uboot-2016-ipq5332_tiny -lk-ipq53xx \
-	fwupgrade-tools
+	fwupgrade-tools sysupgrade-helper
 
 define Target/Description
 	Build firmware image for IPQ53xx SoC devices.
