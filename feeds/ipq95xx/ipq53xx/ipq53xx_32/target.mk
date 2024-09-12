c9088f22c1e810536e31f4c4bf8d33e64c8dd78a
Karim O. <146198118+kmoz000@users.noreply.github.com>
2024-09-12 10:14:20 +0100

    #	new file:   .github/workflows/.config.ipq5332 #	modified:   .github/workflows/build-dev.yml #	new file:   .github/workflows/qsdk.yml #	modified:   feeds/ipq95xx/ipq53xx/ipq53xx_32/config-default #	modified:   feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk #	deleted:    feeds/ipq95xx/ipq53xx/ipq95xx_32/config-default #	deleted:    feeds/ipq95xx/ipq53xx/ipq95xx_32/target.mk #

--- feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk
+++ feeds/ipq95xx/ipq53xx/ipq53xx_32/target.mk
@@ -1,12 +1,12 @@
 
 ARCH:=arm
 SUBTARGET:=ipq53xx_32
-BOARDNAME:=QTI IPQ53xx(32bit) based boards
+BOARDNAME:=QTI IPQ53XX(32bit) based boards
 CPU_TYPE:=cortex-a7
 
 DEFAULT_PACKAGES += \
-	uboot-2016-ipq5332 -uboot-2016-ipq5332_tiny -lk-ipq53xx \
-	fwupgrade-tools
+	uboot-2016-ipq5332 uboot-2016-ipq5332_tiny fwupgrade-tools \
+	kmod-usb-phy-ipq5018 kmod-usb-dwc3-qcom-internal kmod-bt_tty kmod-clk-test
 
 define Target/Description
 	Build firmware image for IPQ53xx SoC devices.
