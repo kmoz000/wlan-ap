bcb5c34cb9afcbcb4bc0976a2b15aae9a0fc45cb
Karim O. <146198118+kmoz000@users.noreply.github.com>
2024-09-27 01:05:26 +0100

    - remove FitzImage

--- feeds/ipq95xx/ipq53xx/image/ipq53xx.mk
+++ feeds/ipq95xx/ipq53xx/image/ipq53xx.mk
@@ -12,11 +12,6 @@ define Device/FitImageLzma
 	KERNEL_NAME := Image
 endef
 
-define Device/FitzImage
-	KERNEL_SUFFIX := -fit-zImage.itb
-	KERNEL = kernel-bin | fit none $$(DTS_DIR)/$$(DEVICE_DTS).dtb
-	KERNEL_NAME := zImage
-endef
 
 define Device/UbiFit
 	KERNEL_IN_UBI := 1
